0b885157c34d092af616fa541b0fb229a677fc88
Amalayarram <45868219+Amalayarram@users.noreply.github.com>
2025-08-12 14:03:47 +0530

    Update Create account.py

--- Create account.py
+++ Create account.py
@@ -1,71 +1,49 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Setup
-driver = webdriver.Firefox()
-driver.implicitly_wait(50)
-driver.maximize_window()
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
-# Step 1: Open Skillcamper website
-driver.get("https://app.skillcamper.com/")
-# driver.get("https://app.skillcamper.com/?_gl=1*151pd4a*_gcl_au*Nzg5MjAzNjQ3LjE3NTMxOTQ2MTk.")
+driver= webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get("https://www.saucedemo.com/")
+# ID
+driver.find_element(By.ID,"user-name").send_keys("standard_user")
+# Using XPath with index
+driver.find_element(By.XPATH,"(//input[@class='input_error form_input'])[2]").send_keys("secret_sauce")
+# Using name
+driver.find_element(By.NAME,"login-button").click()
+# Alert
+# alert =driver.switch_to.alert
+# print(alert.text)
+# alert.accept()
+# driver.implicitly_wait(10)
+# CssSelector path for drop-down
+driver.find_element(By.CSS_SELECTOR,"select[class='product_sort_container']").click()
+# Select the option from the drop-down
+driver.find_element(By.CSS_SELECTOR,"select[class='product_sort_container'] [value='lohi']").click()
+# Click on add-to card
+driver.find_element(By.CSS_SELECTOR,"#add-to-cart-sauce-labs-onesie").click()
 
-
-# Step 2: Click on "Get Started for Free" button
-# driver.find_element(By.XPATH, "//div[@class='uui-navbar_button-wrapper']/a[2]/div[1]").click()
-
-# Step 3: Click on "Sign Up"
-driver.find_element(By.XPATH, "//div[@class='flex gap-4']/button[@class='py-3 px-4 bg-[#000000] text-[#FFFFFF]']").click()
-
-
-# Step 4: Fill the sign-up form
-driver.find_element(By.CSS_SELECTOR, "#form-username").send_keys("Amalaa")
-
-driver.find_element(By.CSS_SELECTOR, "#form-temp-name").send_keys("Yarramm")
-email= driver.find_element(By.CSS_SELECTOR, "#wf-sign-up-email")
-email.send_keys("amalayarram18@gmail.com")
-
-
-driver.find_element(By.CSS_SELECTOR, "#wf-sign-up-password").send_keys("123456789")
-driver.find_element(By.CSS_SELECTOR, "#wf-sign-up-confirm-password").send_keys("123456789")
-
-# Click on "Next" button
-driver.find_element(By.XPATH, "//div[@id='next-button']/span").click()
-
-# it's print the error message of the email id field
-print(driver.find_element(By.XPATH,"//div[@id='email-error']").text)
-
-# Step 5: Wait for 10 seconds
-# time.sleep(10)
-
-# Step 6: Enter phone number and click "Get OTP"
-driver.find_element(By.CSS_SELECTOR, "#wf-sign-up-mobile").send_keys("9916686301")
-
-driver.find_element(By.CSS_SELECTOR, ".getOtpBtn").click()
-
-# Step 7: Wait for 10 seconds
-time.sleep(40)
-
-# Step 8: Click on "Verify & Continue"
-driver.find_element(By.XPATH, "//div[@id='verifyBtn']/span").click()
-
-# Step 9:
-Check = driver.find_element(By.XPATH,"//div[@class='flex gap-2 ']/input[@id='wf-sign-up-accept-privacy']")
-Check.click()
-
-assert Check.is_selected()
+# Click on the Cart option
+driver.find_element(By.CSS_SELECTOR,".shopping_cart_link").click()
 time.sleep(5)
-
-driver.find_element(By.XPATH,"//div[@class='flex gap-2 ']/input[@name='user.attributes.email_marketing']").click()
-time.sleep(15)
-
-# Done
-driver.find_element(By.XPATH,"//button[@id='submit-btn']/span[text()='Submit']").click()
-print("Signup automation completed.")
-# Close the browser (optional)
-# driver.quit()
+# Click on Checking_out button
+driver.find_element(By.XPATH,"//div[@class='cart_footer']/button[text()='Checkout']").click()
+time.sleep(5)
+# User information form
+driver.find_element(By.CSS_SELECTOR,"#first-name").send_keys("Tester")
+driver.find_element(By.CSS_SELECTOR,"#last-name").send_keys("Tester")
+driver.find_element(By.CSS_SELECTOR,"#postal-code").send_keys("522616")
+driver.find_element(By.CSS_SELECTOR,"#continue").click()
+time.sleep(5)
+price=driver.find_element(By.XPATH,"//div[@class='inventory_item_price']").text
+print("Print the price of the cart item "+price)
+# Complete the order
+driver.find_element(By.XPATH,"//div[@class='cart_footer']/button[text()='Finish']").click()
+# Print the order placed and thank you message
+message=driver.find_element(By.XPATH,"//div[@id='checkout_complete_container']/h2[@class='complete-header']").text
+print("Order Placed Successfully Here the message-----"+message)
